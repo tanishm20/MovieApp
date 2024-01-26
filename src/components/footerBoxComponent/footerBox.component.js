f0f7537d0b8054b74f044bee39e44298e75ba6fa
tanish mahajan <tanishmahajan@192.168.1.9>
2024-01-26 22:53:08 +0530

    expand added

--- src/components/footerBoxComponent/footerBox.component.js
+++ src/components/footerBoxComponent/footerBox.component.js
@@ -1,8 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { currentValue, investmentValue } from '../../utils/constants';
 
 const FooterBox = ({ data }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
     const totalCurrentValue = useMemo(() => {
         return data.reduce(
             (accum, item) => accum + currentValue(item?.ltp, item?.quantity),
@@ -17,27 +18,46 @@ const FooterBox = ({ data }) => {
         );
     }, [data]);
 
+    const onPress = () => {
+        setIsExpanded(prev => !prev);
+    };
+
     return (
         <View style={styles.containerStyle}>
+            <Text
+                suppressHighlighting={true}
+                onPress={onPress}
+                style={styles.triangleStyle}>
+                {isExpanded ? '▼' : '▲'}
+            </Text>
+
+            {isExpanded && (
+                <View style={styles.topSpacing}>
+                    <View style={styles.viewStyle}>
+                        <Text style={[styles.boldText, styles.textStyle]}>
+                            Curent Value:
+                        </Text>
+                        <Text style={styles.textStyle}>₹{totalCurrentValue}</Text>
+                    </View>
+                    <View style={styles.viewStyle}>
+                        <Text style={[styles.boldText, styles.textStyle]}>
+                            Total Investment:
+                        </Text>
+                        <Text style={styles.textStyle}>
+                            ₹{totalInvestmentValue.toFixed(2)}
+                        </Text>
+                    </View>
+                    <View style={styles.viewStyle}>
+                        <Text style={[styles.boldText, styles.textStyle]}>
+                            Today's Profit & Loss:
+                        </Text>
+                        <Text style={styles.textStyle}>
+                            ₹{totalCurrentValue - totalInvestmentValue.toFixed(2)}
+                        </Text>
+                    </View>
+                </View>
+            )}
             <View style={styles.viewStyle}>
-                <Text style={[styles.boldText, styles.textStyle]}>Curent Value:</Text>
-                <Text style={styles.textStyle}>₹{totalCurrentValue}</Text>
-            </View>
-            <View style={styles.viewStyle}>
-                <Text style={[styles.boldText, styles.textStyle]}>
-                    Total Investment:
-                </Text>
-                <Text style={styles.textStyle}>₹{totalInvestmentValue.toFixed(2)}</Text>
-            </View>
-            <View style={styles.viewStyle}>
-                <Text style={[styles.boldText, styles.textStyle]}>
-                    Today's Profit & Loss:
-                </Text>
-                <Text style={styles.textStyle}>
-                    ₹{totalCurrentValue - totalInvestmentValue.toFixed(2)}
-                </Text>
-            </View>
-            <View style={[styles.viewStyle, styles.topSpacing]}>
                 <Text style={[styles.boldText, styles.textStyle]}>Profit & Loss:</Text>
                 <Text style={styles.textStyle}>
                     ₹{totalCurrentValue - totalInvestmentValue.toFixed(2)}
@@ -53,7 +73,6 @@ const styles = StyleSheet.create({
     containerStyle: {
         backgroundColor: '#fff',
         paddingHorizontal: 16,
-        paddingVertical: 16,
     },
     viewStyle: {
         paddingVertical: 6,
@@ -67,6 +86,11 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     topSpacing: {
-        paddingTop: 32,
+        marginBottom: 24,
+    },
+    triangleStyle: {
+        alignSelf: 'center',
+        fontSize: 24,
+        color: '#AF4FDC',
     },
 });
